f8997e0a9e10d8db2f569907630ca8e1c3466c2a
javython999 <infinity2544@gmail.com>
2024-03-06 22:39:11 +0900

    BinsearchX

--- src/main/java/basic/searchalgorithm/BinSearch.java
+++ src/main/java/basic/searchalgorithm/BinSearch.java
@@ -26,7 +26,8 @@ public class BinSearch {
         System.out.println("검색할 값: ");
         int ky = stdIn.nextInt();
 
-        int idx = binSearch(x, num, ky);
+        //int idx = binSearch(x, num, ky);
+        int idx = binSearchX(x, num, ky);
 
         if (idx == -1) {
             System.out.println("그 값의 요소가 없습니다.");
@@ -44,6 +45,29 @@ public class BinSearch {
         do {
             int pc = (pl + pr) / 2;
             if (a[pc] == key) {
+                return pc;
+            } else if (a[pc] < key) {
+                pl = pc + 1;
+            } else {
+                pr = pc - 1;
+            }
+        } while (pl <= pr);
+
+        return - 1;
+    }
+
+    static int binSearchX(int[] a, int n, int key) {
+        int pl = 0;
+        int pr = a.length - 1;
+
+        do {
+            int pc = (pl + pr) / 2;
+            if (a[pc] == key) {
+                for (; pc > pl; pc--) {
+                    if (a[pc -1] < key) {
+                        break;
+                    }
+                }
                 return pc;
             } else if (a[pc] < key) {
                 pl = pc + 1;
