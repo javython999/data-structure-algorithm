c104199b1b6bb1bcc47e08192bb3c645f658e9a4
javython999 <infinity2544@gmail.com>
2024-04-01 16:08:28 +0900

    Partition

---
+++ src/main/java/basic/algorithm/Swap.java
@@ -0,0 +1,9 @@
+package basic.algorithm;
+
+public class Swap {
+    public static void swap(int[]a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+}
